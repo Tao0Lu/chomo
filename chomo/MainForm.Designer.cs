63f1b45ae10bb796affcf2ee68165fcfdffe669c
Tao0Lu <2298273530@qq.com>
2021-02-01 20:20:24 +0900

    chomo

---
+++ chomo/MainForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace chomo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.backToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.forwardToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.refreshToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.mainToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.newToolButton = new System.Windows.Forms.ToolStripButton();
+            this.addressTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.goButton = new System.Windows.Forms.ToolStripButton();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.newTabContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.reloadStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeTabContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip.SuspendLayout();
+            this.tabContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backToolStripButton,
+            this.forwardToolStripButton,
+            this.refreshToolStripButton,
+            this.mainToolStripButton,
+            this.newToolButton,
+            this.addressTextBox,
+            this.goButton});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(1009, 31);
+            this.toolStrip.TabIndex = 5;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // backToolStripButton
+            // 
+            this.backToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.backToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("backToolStripButton.Image")));
+            this.backToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.backToolStripButton.Name = "backToolStripButton";
+            this.backToolStripButton.Size = new System.Drawing.Size(29, 28);
+            this.backToolStripButton.Text = "backToolStripButton";
+            this.backToolStripButton.ToolTipText = "返回";
+            this.backToolStripButton.Click += new System.EventHandler(this.backToolStripButton_Click);
+            // 
+            // forwardToolStripButton
+            // 
+            this.forwardToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.forwardToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("forwardToolStripButton.Image")));
+            this.forwardToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.forwardToolStripButton.Name = "forwardToolStripButton";
+            this.forwardToolStripButton.Size = new System.Drawing.Size(29, 28);
+            this.forwardToolStripButton.Text = "forwardToolStripButton";
+            this.forwardToolStripButton.ToolTipText = "前进";
+            this.forwardToolStripButton.Click += new System.EventHandler(this.forwardToolStripButton_Click);
+            // 
+            // refreshToolStripButton
+            // 
+            this.refreshToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.refreshToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("refreshToolStripButton.Image")));
+            this.refreshToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.refreshToolStripButton.Name = "refreshToolStripButton";
+            this.refreshToolStripButton.Size = new System.Drawing.Size(29, 28);
+            this.refreshToolStripButton.Text = "refreshToolStripButton";
+            this.refreshToolStripButton.ToolTipText = "刷新";
+            this.refreshToolStripButton.Click += new System.EventHandler(this.refreshToolStripButton_Click);
+            // 
+            // mainToolStripButton
+            // 
+            this.mainToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.mainToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("mainToolStripButton.Image")));
+            this.mainToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mainToolStripButton.Name = "mainToolStripButton";
+            this.mainToolStripButton.Size = new System.Drawing.Size(29, 28);
+            this.mainToolStripButton.Text = "mainToolStripButton";
+            this.mainToolStripButton.ToolTipText = "打开主页";
+            this.mainToolStripButton.Click += new System.EventHandler(this.mainToolStripButton_Click);
+            // 
+            // newToolButton
+            // 
+            this.newToolButton.BackColor = System.Drawing.Color.Transparent;
+            this.newToolButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.newToolButton.Image = ((System.Drawing.Image)(resources.GetObject("newToolButton.Image")));
+            this.newToolButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newToolButton.Name = "newToolButton";
+            this.newToolButton.Size = new System.Drawing.Size(29, 28);
+            this.newToolButton.Text = "&New";
+            this.newToolButton.ToolTipText = "新建标签页";
+            this.newToolButton.Click += new System.EventHandler(this.newToolButton_Click);
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.AutoSize = false;
+            this.addressTextBox.Font = new System.Drawing.Font("苹方字体", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(830, 31);
+            this.addressTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.addressTextBox_KeyPress);
+            // 
+            // goButton
+            // 
+            this.goButton.BackColor = System.Drawing.Color.Transparent;
+            this.goButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.goButton.Image = ((System.Drawing.Image)(resources.GetObject("goButton.Image")));
+            this.goButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(29, 28);
+            this.goButton.Text = "前往";
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 31);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.ShowToolTips = true;
+            this.tabControl.Size = new System.Drawing.Size(1009, 603);
+            this.tabControl.TabIndex = 6;
+            this.tabControl.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged);
+            this.tabControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.tabControl_MouseUp);
+            // 
+            // tabContextMenu
+            // 
+            this.tabContextMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.tabContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newTabContextMenuItem,
+            this.toolStripMenuItem1,
+            this.reloadStripMenuItem,
+            this.toolStripMenuItem2,
+            this.closeTabContextMenuItem});
+            this.tabContextMenu.Name = "tabContextMenu";
+            this.tabContextMenu.Size = new System.Drawing.Size(166, 100);
+            // 
+            // newTabContextMenuItem
+            // 
+            this.newTabContextMenuItem.Name = "newTabContextMenuItem";
+            this.newTabContextMenuItem.Size = new System.Drawing.Size(165, 28);
+            this.newTabContextMenuItem.Text = "新建标签页";
+            this.newTabContextMenuItem.Click += new System.EventHandler(this.newToolButton_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(162, 6);
+            // 
+            // reloadStripMenuItem
+            // 
+            this.reloadStripMenuItem.Name = "reloadStripMenuItem";
+            this.reloadStripMenuItem.Size = new System.Drawing.Size(165, 28);
+            this.reloadStripMenuItem.Text = "刷新";
+            this.reloadStripMenuItem.Click += new System.EventHandler(this.refreshToolStripButton_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(162, 6);
+            // 
+            // closeTabContextMenuItem
+            // 
+            this.closeTabContextMenuItem.Name = "closeTabContextMenuItem";
+            this.closeTabContextMenuItem.Size = new System.Drawing.Size(165, 28);
+            this.closeTabContextMenuItem.Text = "关闭标签页";
+            this.closeTabContextMenuItem.Click += new System.EventHandler(this.closeTabContextMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 634);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.toolStrip);
+            this.Name = "MainForm";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.tabContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton backToolStripButton;
+        private System.Windows.Forms.ToolStripButton forwardToolStripButton;
+        private System.Windows.Forms.ToolStripButton refreshToolStripButton;
+        private System.Windows.Forms.ToolStripButton mainToolStripButton;
+        private System.Windows.Forms.ToolStripButton newToolButton;
+        private System.Windows.Forms.ToolStripButton goButton;
+        private System.Windows.Forms.ToolStripTextBox addressTextBox;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.ContextMenuStrip tabContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem newTabContextMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem reloadStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem closeTabContextMenuItem;
+    }
+}
+
---
+++ chomo/MainForm.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System.Windows.Forms;
+using System.Linq;
+using System.Resources;
+using System.Drawing;
+
+namespace chomo
+{
+    public partial class MainForm : Form
+    {
+        public string startUrl = "https://www.baidu.com/";
+        private readonly string _mainTitle;
+        Image CloseImage = (Image)new ResourceManager("chomo.MainForm", System.Reflection.Assembly.GetExecutingAssembly()).GetObject("close");
+        Image RefreshImage = (Image)new ResourceManager("chomo.MainForm", System.Reflection.Assembly.GetExecutingAssembly()).GetObject("refresh");
+        private int size;
+        public MainForm()
+        {
+            InitializeComponent();
+            NewTab(startUrl);
+            _mainTitle = "Chomo";
+            size = 18;
+        }
+
+        private async void NewTab(string startUrl)
+        {
+            TabPage page = new TabPage("新标签页") { Padding = new Padding(0, 0, 0, 0) };
+            WebView2 browser = new WebView2
+            {
+                Source = new Uri(startUrl),
+                Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom,
+                Dock = DockStyle.Fill
+            };
+            CoreWebView2Environment ev = await CoreWebView2Environment.CreateAsync(null, AppContext.BaseDirectory);
+            await browser.EnsureCoreWebView2Async(ev);
+            CoreWebView2 coreWebView2 = browser.CoreWebView2;
+
+            //Event Start
+
+            coreWebView2.SourceChanged += (s, e) =>
+            {
+                BeginInvoke(new Action(() =>
+                {
+                    addressTextBox.Text = coreWebView2.Source;
+                }));
+            };
+            coreWebView2.DocumentTitleChanged += (s, e) =>
+            {
+                BeginInvoke(new Action(() =>
+                {
+                    var title = coreWebView2.DocumentTitle;
+                    if (title != null)
+                    {
+                        if (tabControl.SelectedTab == page)
+                        {
+                            Text = coreWebView2.DocumentTitle + " - " + _mainTitle;
+                        }
+                        page.ToolTipText = title;
+                        if (title.Length > size)
+                        {
+                            title = title.Substring(0, size) + "...";
+                        }
+                        page.Text = title;
+                    }
+
+                }));
+            };
+            refreshToolStripButton.Image = CloseImage;
+            refreshToolStripButton.Click += stopRefreshToolStripButton_Click;
+            coreWebView2.NavigationCompleted += (s, e) =>
+            {
+                refreshToolStripButton.Image = RefreshImage;
+                refreshToolStripButton.Click += refreshToolStripButton_Click;
+            };
+            coreWebView2.NewWindowRequested += OnNewWindowRequested;
+
+            page.Controls.Add(browser);
+            tabControl.TabPages.Add(page);
+            tabControl.SelectedTab = page;
+        }
+
+        private void OnNewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true;
+            NewTab(e.Uri);
+        }
+
+        private void GoAddress(string url)
+        {
+            var ctl = GetAliveBrowser();
+            if (ctl != null)
+            {
+                try
+                {
+                    ctl.Source = new Uri(url);
+                }
+                catch (UriFormatException)
+                {
+                    if (url.Substring(8).ToLower() != "https://" || url.Substring(7).ToLower() != "http://")
+                    {
+                        ctl.Source = new Uri("https://" + url);
+                    }
+                    else
+                    {
+                        MessageBox.Show(url + "不是一个正确的url。");
+                    }
+
+                }
+            }
+            else
+            {
+                NewTab(addressTextBox.Text);
+            }
+        }
+
+        private WebView2 GetAliveBrowser()
+        {
+            if (tabControl.TabCount > 0)
+            {
+                TabPage page = tabControl.TabPages[tabControl.SelectedIndex];
+                return page.Controls.OfType<WebView2>().FirstOrDefault();
+            }
+            return null;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            GoAddress(addressTextBox.Text);
+        }
+
+        private void newToolButton_Click(object sender, EventArgs e)
+        {
+            NewTab(startUrl);
+        }
+
+        private void addressTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                GoAddress(addressTextBox.Text);
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            addressTextBox.Width = toolStrip.Width - 30 * 5;
+            size = (int)(this.Width / (1.5 * tabControl.Font.Size) / tabControl.TabCount);
+            if (tabControl.TabCount > 0)
+            {
+                var page = tabControl.TabPages[tabControl.SelectedIndex];
+                foreach (var crl in page.Controls)
+                {
+                    WebView2 browser = crl as WebView2;
+                    CoreWebView2 coreWebView2 = browser.CoreWebView2;
+                    var title = coreWebView2.DocumentTitle;
+                    if (title != null)
+                    {
+                        page.ToolTipText = title;
+                        if (title.Length > size)
+                        {
+                            title = title.Substring(0, size) + "...";
+                        }
+                        page.Text = title;
+                    }
+                }
+            }
+        }
+
+        private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl.TabCount > 0)
+            {
+                var page = tabControl.TabPages[tabControl.SelectedIndex];
+                foreach (var crl in page.Controls)
+                {
+                    WebView2 browser = crl as WebView2;
+                    Text = browser.CoreWebView2.DocumentTitle + " - " + _mainTitle;
+                    addressTextBox.Text = browser.CoreWebView2.Source;
+                }
+            }
+            else
+            {
+                Text = _mainTitle;
+            }
+        }
+
+        private void backToolStripButton_Click(object sender, EventArgs e)
+        {
+            WebView2 browser = GetAliveBrowser();
+            if (browser.CoreWebView2 != null)
+            {
+                browser.CoreWebView2.GoBack();
+            }
+        }
+
+        private void forwardToolStripButton_Click(object sender, EventArgs e)
+        {
+            WebView2 browser = GetAliveBrowser();
+            if (browser.CoreWebView2 != null)
+            {
+                browser.CoreWebView2.GoForward();
+            }
+        }
+
+        private void refreshToolStripButton_Click(object sender, EventArgs e)
+        {
+            refreshToolStripButton.Image = CloseImage;
+            refreshToolStripButton.Click += stopRefreshToolStripButton_Click;
+            WebView2 browser = GetAliveBrowser();
+            if (browser.CoreWebView2 != null)
+            {
+                browser.CoreWebView2.Reload();
+                browser.CoreWebView2.NavigationCompleted += (s, d) =>
+                {
+                    BeginInvoke(new Action(() =>
+                    {
+                        refreshToolStripButton.Image = RefreshImage;
+                        refreshToolStripButton.Click += refreshToolStripButton_Click;
+                    }));
+                };
+            }
+        }
+        private void stopRefreshToolStripButton_Click(object sender, EventArgs e)
+        {
+            WebView2 browser = GetAliveBrowser();
+            if (browser.CoreWebView2 != null)
+            {
+                browser.CoreWebView2.Stop();
+            }
+            refreshToolStripButton.Image = RefreshImage;
+            refreshToolStripButton.Click += refreshToolStripButton_Click;
+        }
+
+        private void closeTabContextMenuItem_Click(object sender, EventArgs e)
+        {
+                ToolStripMenuItem s = (ToolStripMenuItem)sender;
+                TabPage page = s.Tag as TabPage;
+                if (page != null)
+                {
+                    page.Dispose();
+                }
+        }
+
+        private void tabControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                for (int i = 0; i < tabControl.TabCount; i++)
+                {
+                    Rectangle r = tabControl.GetTabRect(i);
+                    if (r.Contains(e.Location))
+                    {
+                        closeTabContextMenuItem.Tag = tabControl.TabPages[i];
+                        reloadStripMenuItem.Tag = tabControl.TabPages[i];
+                        tabContextMenu.LostFocus += (s, ev) => { tabContextMenu.Hide(); };
+                        tabContextMenu.ChangeUICues += (s, ev) => { tabContextMenu.Hide(); };
+                        tabContextMenu.Show(tabControl, e.Location);
+                    }
+                }
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                for (int i = 0; i < tabControl.TabCount; i++)
+                {
+                    Rectangle r = tabControl.GetTabRect(i);
+                    if (r.Contains(e.Location))
+                    {
+                        TabPage page = tabControl.TabPages[i];
+                        if (page != null)
+                        {
+                            page.Dispose();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void mainToolStripButton_Click(object sender, EventArgs e)
+        {
+            WebView2 browser = GetAliveBrowser();
+            if (browser.CoreWebView2 != null)
+            {
+                browser.Source = new Uri(startUrl);
+            }
+        }
+    }
+}
+    
